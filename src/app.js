eb8bf6dcf694dd3491ff5194823ac2c7da310a65
sota1235 <sota1235@gmail.com>
2016-10-26 10:38:08 +0900

    modify: add html page

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-import 'babel-polyfill';
+/**
+ * @fileoverview Sample of draft.js
+ */
 
-const sleep = msec => new Promise(resolve => {
-  setTimeout(resolve, msec);
-});
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Editor, EditorState} from 'draft-js';
 
-(async () => {
-  console.log('start');
-  await sleep (2000);
-  console.log('end');
-})();
+class MyEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editorState: EditorState.createEmpty(),
+    };
+    this.onChange = editorState => this.setState({editorState});
+  }
+
+  render() {
+    const {editorState} = this.state;
+
+    return <Editor editorState={editorState} onChange={this.onChange} />;
+  }
+}
+
+ReactDOM.render(
+  <MyEditor />,
+  document.getElementById('container')
+);
